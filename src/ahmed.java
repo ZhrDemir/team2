02deca37df266f79248ff1d31412951b85554bf3
Asmuftu <firex8@icloud.com>
2023-12-17 12:10:22 +0300

    update on 17.12.2023

--- src/ahmed.java
+++ src/ahmed.java
@@ -3,5 +3,6 @@ public class ahmed {
     public static void main(String[] args) {
         System.out.println("Ahmed'in class'i");
         System.out.println("update 14.12.2023 22.07");
+        System.out.println("update 17.12.2023 12.09");
     }
 }
